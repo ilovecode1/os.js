cd16eb5420279f4bd6a77d62325df1831e84d9b1
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2015-03-24 15:31:39 -0400

    0.3.5
    Smarter and minified!

--- os.js
+++ os.js
@@ -1,53 +1,8 @@
 /*
 
-os.js v0.2 by cosmic open source projects
+os.js v0.3.5 by cosmic open source projects
 
 */
 
-windows = false;
-ios = false;
-macos = false;
-unix = false;
-linux = false;
-ios = false;
-android = false;
-  
-
-if (navigator.appVersion.indexOf("Win")!=-1) 
-{
-  
-windows = true;
-
-}
-
-else if (navigator.appVersion.indexOf("Mac")!=-1)
-{
-
-macos = true
-
-}
-
-else if (navigator.appVersion.indexOf("X11")!=-1)
-{
-  
-  unix = true;
-} 
-
-else if (navigator.appVersion.indexOf("Linux")!=-1)
-{
-  
- linux = true;
-  
-}
-
-else if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) )
-  {
-    ios = true;
-
-  }
-else if( userAgent.match( /Android/i ) )
-  {
-
-    android = true;
-  }
+var _0x47d9=["\x57\x69\x6E","\x69\x6E\x64\x65\x78\x4F\x66","\x61\x70\x70\x56\x65\x72\x73\x69\x6F\x6E","\x4D\x61\x63","\x58\x31\x31","\x4C\x69\x6E\x75\x78","\x6D\x61\x74\x63\x68"];var _0x78b7=[_0x47d9[0],_0x47d9[1],_0x47d9[2],_0x47d9[3],_0x47d9[4],_0x47d9[5],_0x47d9[6]];var _0x1101=[_0x78b7[0],_0x78b7[1],_0x78b7[2],_0x78b7[3],_0x78b7[4],_0x78b7[5],_0x78b7[6]];var _0xb6ca=[_0x1101[0],_0x1101[1],_0x1101[2],_0x1101[3],_0x1101[4],_0x1101[5],_0x1101[6]];var _0x3086=[_0xb6ca[0],_0xb6ca[1],_0xb6ca[2],_0xb6ca[3],_0xb6ca[4],_0xb6ca[5],_0xb6ca[6]];windows=false;ios=false;macos=false;unix=false;linux=false;ios=false;android=false;if(navigator[_0x3086[2]][_0x3086[1]](_0x3086[0])!= -1){windows=true}else {if(navigator[_0x3086[2]][_0x3086[1]](_0x3086[3])!= -1){macos=true}else {if(navigator[_0x3086[2]][_0x3086[1]](_0x3086[4])!= -1){unix=true}else {if(navigator[_0x3086[2]][_0x3086[1]](_0x3086[5])!= -1){linux=true}else {if(userAgent[_0x3086[6]](/iPad/i)||userAgent[_0x3086[6]](/iPhone/i)||userAgent[_0x3086[6]](/iPod/i)){ios=true}else {if(userAgent[_0x3086[6]](/Android/i)){android=true}}}}}};
   
